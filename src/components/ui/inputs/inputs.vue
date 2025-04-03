e1bd55886857d2ba1e9beca3e4e6f22ccbdf6bbe
KotoFeelGood72 <kotenkolove@yandex.ru>
2025-04-03 10:13:14 +0300

    s

--- src/components/ui/inputs/inputs.vue
+++ src/components/ui/inputs/inputs.vue
@@ -25,7 +25,7 @@ const props = withDefaults(
     placeholder?: string;
     message?: string;
     error?: boolean;
-    icon: string;
+    icon?: string;
     modelValue: string;
     label?: string;
     disabled?: boolean;
